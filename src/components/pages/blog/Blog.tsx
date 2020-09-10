8f534e32c2af894933023596f91e30f3a0b19497
tonyvugithub <55034244+tonyvugithub@users.noreply.github.com>
2020-09-10 10:45:23 -0400

    Update blog page

--- src/components/pages/blog/Blog.tsx
+++ src/components/pages/blog/Blog.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-const BlogWrapper = styled(motion.div)`
+interface BlogWrapperProps {
+  imgUrl: string;
+}
+
+const BlogWrapper = styled(motion.div)<BlogWrapperProps>`
   padding-top: 100px;
   text-align: center;
+  height: calc(100vh - 195px);
+  width: 100%;
+  color: #fff;
 
-  @media (min-width: 768px) {
-    max-width: 1200px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.85)),
+    url(${(p) => p.imgUrl});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  position: relative;
+
+  div {
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
   }
 `;
 
 const Blog = () => {
   return (
-    <BlogWrapper>
+    <BlogWrapper imgUrl={process.env.PUBLIC_URL + '/img/blog.jpg'}>
       <div>This feature is in development</div>
     </BlogWrapper>
   );
